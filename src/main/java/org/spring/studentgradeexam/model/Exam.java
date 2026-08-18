288bcc25f2f48a4d69d14eb228ed75e6f75d1b0a
fatratra-png <nyfatratrar@gmail.com>
2026-08-18 10:57:45 +0300

    feat:add getAverage method that returns the average of all grades and round it to 2 digit past the comma

--- src/main/java/org/spring/studentgradeexam/model/Exam.java
+++ src/main/java/org/spring/studentgradeexam/model/Exam.java
@@ -12,4 +12,9 @@ public record Exam(String id,String title,Instant examDate,int coefficient,List<
     public List<Student> getStudentsWithGradeBetween(BigDecimal min,BigDecimal max){
         return this.examGrades.stream().filter(grade->grade.score().compareTo(min)>=0 && grade.score().compareTo(max)<=0).map(Grade::student).toList();
     }
+
+    public BigDecimal getAverage(){
+        BigDecimal sum = this.examGrades.stream().map(Grade::score).reduce(BigDecimal.ZERO, BigDecimal::add);
+        return sum.divide(BigDecimal.valueOf(this.examGrades.size()),2,BigDecimal.ROUND_HALF_UP);
+    }
 }
